179873bf703a65f56d807f7af1946c41096fcc22
TiredSteam <petersark@tiredsteam.com>
2022-04-20 12:26:24 -0500

    Update intro.js

--- src/components/intro.js
+++ src/components/intro.js
@@ -7,19 +7,19 @@ export default function Intro() {
         Blog.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
+        Discover Your Radiant Life with{' '}
         <a
-          href="https://www.gatsbyjs.com/"
+          href="https://www.radiantlifespa.com/"
           className="underline hover:text-success duration-200 transition-colors"
         >
-          Gatsby
+          Dr Mike Barber
         </a>{' '}
         and{' '}
         <a
-          href="https://www.datocms.com/"
+          href="https://www.radiantlifespa.com/"
           className="underline hover:text-success duration-200 transition-colors"
         >
-          DatoCMS
+          Ramona DeSmith
         </a>
         .
       </h4>
