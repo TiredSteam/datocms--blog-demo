91e6ebd7b0b4f398ec793c54be06ba0c8f24f7f5
TiredSteam <petersark@tiredsteam.com>
2023-04-24 14:24:47 -0500

    Update intro.js

--- src/components/intro.js
+++ src/components/intro.js
@@ -4,41 +4,9 @@ export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Insights
+        SRC Benefits News
       </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        Discover Your Radiant Life with{' '}
-        <a
-          href="https://www.radiantlifespa.com/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Dr Barber
-        </a>{' '}
-        and{' '}
-        <a
-          href="https://radiantlifespa.com/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Ramona DeSmith
-        </a>
-        .
-      </h4>
-      <h5 className="text-center md:text-left text-sm mt-5 md:pl-8">
-      <a
-          href="https://www.radiantlifespa.com/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Dr Barber
-        </a>{' '}
-        and{' '}
-        <a
-          href="https://radiantlifespa.com/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Ramona DeSmith
-        </a>
-
-      </h5>
+    
     <div class="flex justify-center">
       <a href="#!" class="mr-6 text-gray-600">
         <svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-f"
