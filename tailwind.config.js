edc4bfc58976bf871125f51abe8928c086a5ae49
TiredSteam <petersark@tiredsteam.com>
2023-05-11 13:30:39 -0500

    tailwindtest

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,47 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {
-      colors: {
-        "accent-1": "#FAFAFA",
-        "accent-2": "#EAEAEA",
-        "accent-7": "#333",
-        success: "#0070f3",
-        cyan: "#79FFE1",
-      },
-      spacing: {
-        28: "7rem",
-      },
-      letterSpacing: {
-        tighter: "-.04em",
-      },
-      lineHeight: {
-        tight: 1.2,
-      },
-      fontSize: {
-        "4xl" : "1.75rem",
-        "5xl": "2.5rem",
-        "6xl": "2.75rem",
-        "7xl": "4.5rem",
-        "8xl": "6.25rem",
-      },
-      boxShadow: {
-        small: "0 5px 10px rgba(0, 0, 0, 0.12)",
-        medium: "0 8px 30px rgba(0, 0, 0, 0.12)",
+      typography(theme) {
+        return {
+          dark: {
+            css: {
+              color: theme("colors.gray.300"),
+              '[class~="lead"]': { color: theme("colors.gray.400") },
+              a: { color: theme("colors.gray.100") },
+              strong: { color: theme("colors.gray.100") },
+              "ul > li::before": { backgroundColor: theme("colors.gray.700") },
+              hr: { borderColor: theme("colors.gray.800") },
+              blockquote: {
+                color: theme("colors.gray.100"),
+                borderLeftColor: theme("colors.gray.800"),
+              },
+              h1: { color: theme("colors.gray.100") },
+              h2: { color: theme("colors.gray.100") },
+              h3: { color: theme("colors.gray.100") },
+              h4: { color: theme("colors.gray.100") },
+              code: { color: theme("colors.gray.100") },
+              "a code": { color: theme("colors.gray.100") },
+              pre: {
+                color: theme("colors.gray.200"),
+                backgroundColor: theme("colors.gray.800"),
+              },
+              thead: {
+                color: theme("colors.gray.100"),
+                borderBottomColor: theme("colors.gray.700"),
+              },
+              "tbody tr": { borderBottomColor: theme("colors.gray.800") },
+            },
+          },
+        };
       },
     },
   },
+
   variants: {
-    extend: {},
+    extend: { typography: ["dark"] }
   },
+
   plugins: [require("@tailwindcss/typography")],
+
+  darkMode: "media",
 };
