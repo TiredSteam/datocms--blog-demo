c4fc6a3164ddd65f5e0d529452adf1d7aeac2561
TiredSteam <petersark@tiredsteam.com>
2023-05-11 12:17:04 -0500

    prsupdt2

--- src/components/post-body.js
+++ src/components/post-body.js
@@ -5,7 +5,7 @@ import { StructuredText } from "react-datocms";
 export default function PostBody({ content }) {
   return (
     <div className="max-w-2xl mx-auto">
-      <div className="prose prose-lg  dark:prose-invert">
+      <div className="prose prose-lg prose-h3:text-gray-500 prose-p:text-gray-200 dark:prose-invert">
         <StructuredText
           data={content}
           renderBlock={({ record }) => {
